f8024350305a644eb708f91cead221b88d313fbd
Cap0011 <59128435+Cap0011@users.noreply.github.com>
2024-03-25 23:27:11 +0900

    Initial commit

---
+++ sevenOclock/sevenOclock/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sevenOclock
+//
+//  Created by Jiyoung Park on 2024/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sevenOclock/sevenOclock/sevenOclockApp.swift
@@ -0,0 +1,17 @@
+//
+//  sevenOclockApp.swift
+//  sevenOclock
+//
+//  Created by Jiyoung Park on 2024/03/25.
+//
+
+import SwiftUI
+
+@main
+struct sevenOclockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
